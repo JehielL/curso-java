632e3bc0e2d1758dc64d489399652276622cb767
JehielL <jehiel.py@gmail.com>
2023-11-21 18:40:50 +0100

    Cambio directorio

---
+++ html/script.js
@@ -0,0 +1,9 @@
+function toggleImage(){
+    let showImage =  document.getElementById('retrete');
+       if (showImage.style.display === 'none'){ 
+        showImage.style.display = 'block';
+    
+    } else {
+        showImage.style.display = 'none';
+    }
+ }
