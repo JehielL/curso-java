4a9a465923793475755a6ef107c3dcb65e069f94
JehielL <jehiel.py@gmail.com>
2023-11-23 19:20:37 +0100

    Agregado nuevo Ecommerce, funciones dark theme.

--- html/script.js
+++ html/script.js
@@ -7,3 +7,19 @@ function toggleImage(){
         showImage.style.display = 'none';
     }
  }
+
+
+ function toggleTheme() {
+    const body = document.body;
+    const currentTheme = body.getAttribute('data-bs-theme');
+    const logo = document.getElementById('toggleTheme'); 
+    if (currentTheme === 'dark') {
+        body.setAttribute('data-bs-theme', 'light');
+        logo.classList.remove('fa-regular', 'fa-sun', 'ms-2', 'rounded-5');
+        logo.classList.add('fa-solid', 'fa-moon', 'ms-2', 'rounded-5');
+    } else {
+        body.setAttribute('data-bs-theme', 'dark');
+        logo.classList.remove('fa-solid', 'fa-moon', 'ms-2', 'rounded-5');
+        logo.classList.add('fa-regular', 'fa-sun', 'ms-2', 'rounded-5');
+    }
+}
