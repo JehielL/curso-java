dbab4130ec15dbfdbed84f10d54a09dd9b2f38c7
JehielL <jehiel.py@gmail.com>
2023-12-12 19:18:45 +0100

    Python operadores.

--- Python/001.operadores.py
+++ Python/001.operadores.py
@@ -27,3 +27,62 @@ if es_estudiante or precio >= 100:
     print(f"Descuento del 20%, precio final: {precio * 0.80}")
 else:
     print(f"Precio Final {precio}")
+    
+##NOT invierte una condición existente.
+
+
+edad = int(input('Introduce tu edad : '))
+if not edad >= 18:
+    print('No tiene Acceso.')
+    
+##operadores de identidad 
+
+email = None
+
+email_user = input('Introduce tu email: ')
+
+if email_user.endswith('gmail.com'):
+    email = email_user
+    
+if email is None:
+    print('No se ha podido completar el registro')
+
+
+#is not
+
+if email is not None:
+    print('El email es correcto , registro completado')
+    
+#if else
+
+if email is None:
+    print('No se ha podido completar el registro')
+else:
+    print('El email es correcto, registro completado')
+    
+# operadores de membresia
+
+#in con textos 
+
+print('java' in 'curso avanzado de java con spring')
+
+#in en listas
+
+students = ['jehiel', 'noemi', 'judith', 'carlos',' ronald']
+name = input("introduce tu nombre: ")
+
+if name in students :
+    print('Tienes acceso al curso')
+else:
+    print('No tienes acceso')
+
+
+
+
+# not in
+
+if name not in students:
+    print('No puedes entrar')
+else:
+    print('Puedes entrar al curso de Java')
+    
