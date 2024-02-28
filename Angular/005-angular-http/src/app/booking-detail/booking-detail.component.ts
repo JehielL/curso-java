ee0c13c9d42561861e0b2823b7205738a7b32775
JehielL <jehiel.py@gmail.com>
2024-02-28 17:53:21 +0100

    Conexion http con backend

---
+++ Angular/005-angular-http/src/app/booking-detail/booking-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-detail.component.html',
+  styleUrl: './booking-detail.component.css'
+})
+export class BookingDetailComponent {
+
+}
