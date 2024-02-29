338192761a494555df1ee9bb75fe6c513c16d48a
JehielL <jehiel.py@gmail.com>
2024-02-29 16:19:03 +0100

    Agregado y enrutado modelo user.

--- Angular/005-angular-http/src/app/app.routes.ts
+++ Angular/005-angular-http/src/app/app.routes.ts
@@ -2,14 +2,24 @@ import { Routes } from '@angular/router';
 
 import { BookingListComponent } from './booking-list/booking-list.component';
 import { BookingDetailComponent } from './booking-detail/booking-detail.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 export const routes: Routes = [
  {
-    path: '',
+    path: 'bookings',
     component: BookingListComponent
  },
  {
    path: 'bookings/:id/detail',
    component: BookingDetailComponent
- }
+ },
+ {
+  path: 'users/:id/detail',
+  component: UserDetailComponent
+},
+{
+  path: 'users',
+  component: UserListComponent
+}
 ];
