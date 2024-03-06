323514cb685773425a4233db0bd3ab69c6a88795
JehielL <jehiel.py@gmail.com>
2024-03-06 16:57:08 +0100

    Angular formulario de registro agregado.

--- Angular/005-angular-http/src/app/register/register.component.ts
+++ Angular/005-angular-http/src/app/register/register.component.ts
@@ -1,12 +1,38 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Register } from '../../interfaces/register.mode';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  registerForm = this.fb.group({
+
+    email:[''],
+    phone:[''],
+    password:['']
+
+  })
+
+  constructor(private fb: FormBuilder, private httpClient: HttpClient){}
+
+  save(){
+
+    const register: Register = {
+
+      email: this.registerForm.get('email')?.value ?? '',
+      phone: this.registerForm.get('phone')?.value ?? '',
+      password: this.registerForm.get('password')?.value ?? ''
+    }
+
+    console.log(register)
+
+
+  }
 }
---
+++ Angular/005-angular-http/src/interfaces/register.mode.ts
@@ -0,0 +1,6 @@
+export interface Register{
+
+    email: string;
+    phone: string;
+    password: string;
+}
