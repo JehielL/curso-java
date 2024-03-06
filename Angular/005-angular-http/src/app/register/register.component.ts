c898f8b6fbd374a6eb15b850c8925995ffc2dfe3
JehielL <jehiel.py@gmail.com>
2024-03-06 20:38:22 +0100

    Agregada caja de imagen form booking.

--- Angular/005-angular-http/src/app/register/register.component.ts
+++ Angular/005-angular-http/src/app/register/register.component.ts
@@ -2,11 +2,12 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Register } from '../../interfaces/register.model';
+import { RouterEvent, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [HttpClientModule, ReactiveFormsModule],
+  imports: [HttpClientModule, ReactiveFormsModule,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -16,8 +17,8 @@ export class RegisterComponent {
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{9}$')]),
-    password: new FormControl(''),
-    passwordConfirm: new FormControl('')
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]),
+    passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(30)])
   },
     { validators: this.passwordConfirmValidator } // Validador personalizado que comprueba dos campos al mismo tiempo
   );
