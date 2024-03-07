16621f6229523ec813767ce6788a24fe433da470
JehielL <jehiel.py@gmail.com>
2024-03-07 18:17:09 +0100

    Full stack booking-list mostrar lista de reservas desde base de datos al frontend.

--- Fullstack/001-fullstack/backend/src/main/java/com/bitebooking/controller/BookingController.java
+++ Fullstack/001-fullstack/backend/src/main/java/com/bitebooking/controller/BookingController.java
@@ -3,19 +3,27 @@ package com.bitebooking.controller;
 import com.bitebooking.model.Booking;
 import com.bitebooking.repository.BookingRepository;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @AllArgsConstructor
+@Slf4j
+
 public class BookingController {
 
     private final BookingRepository repo;
 
     @GetMapping("bookings")
     public List<Booking> findAll(){
+        log.info("REST request to findAll Bookings");
 
         return this.repo.findAll();
     }
--- Fullstack/001-fullstack/backend/src/main/java/com/bitebooking/model/Booking.java
+++ Fullstack/001-fullstack/backend/src/main/java/com/bitebooking/model/Booking.java
@@ -5,11 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Booking {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
